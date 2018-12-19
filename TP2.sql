12425378efd5a95341524afc255b90c7553e6d27
ARMANET Nathan <nathan.armanet@gmail.com>
2018-12-19 14:53:43 +0100

    suppression des tables terminé

--- TP2.sql
+++ TP2.sql
@@ -30,15 +30,37 @@ select * from USER_CONSTRAINTS;
 -- 1
 insert into FOURNISSEUR(nomfour, statut, ville) VALUES ('BonVin', 'EARL', 'Bordeaux');
 -- => ORA-00001: violation de contrainte unique (INI3A03.PK_FOURNISSEUR)
+-- Le fournisseur BonVin existe déjà
 
 -- 2
 insert into FOURNISSEUR(nomfour, statut, ville) VALUES  ('VinBon' , 'SARL', 'Dijon');
 -- => insertion valide
 
 -- 3
-insert into FOURNISSEUR(nomfour, statut, ville) VALUES  ('VinBon' , 'SARL', 'Dijon');
--- => ORA-00001: violation de contrainte unique (INI3A03.PK_FOURNISSEUR)
+delete from FOURNISSEUR
+  where NOMFOUR= 'BonVin'
+        AND STATUT = 'SARL'
+        AND VILLE = 'Dijon';
+-- ORA-02292: violation de contrainte (INI3A03.FK_PRODUIT) d'intégrité - enregistrement fils existant
+-- Le fournisseur BonVin avait des produits encore enregistrés dans la table PRODUIT
 
 -- 4
 insert into PRODUIT(appellation, nomfour, prix) values  ('Bordeaux' , 'Chapoutier', '20');
 -- ORA-00001: violation de contrainte unique (INI3A03.PK_PRODUIT)
+-- le couple (Bordeaux, Chapoutier) existe déjà
+
+-- AUTRES !!!
+
+-- Entrer les données dans un ordre précis
+-- 1. FOURNISSEUR
+-- 2. PRODUIT
+-- 3. COMMANDE
+
+--Partie 2
+
+-- 1
+drop view ...
+drop table ...
+select * from USER_TABLES;
+select * from USER_VIEWS;
+select * from USER_OBJECTS;
