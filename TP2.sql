f94c820093dd8108f364261911b73ec17c884b77
ARMANET Nathan <nathan.armanet@gmail.com>
2018-12-19 14:31:47 +0100

    implementation d'une partie des tests

--- TP2.sql
+++ TP2.sql
@@ -24,3 +24,21 @@ FOREIGN KEY (APPELLATION, NOMFOUR) REFERENCES PRODUIT(APPELLATION, NOMFOUR);
 
 -- Vérification
 select * from USER_CONSTRAINTS;
+
+-- Test contraintes d'intégrité
+
+-- 1
+insert into FOURNISSEUR(nomfour, statut, ville) VALUES ('BonVin', 'EARL', 'Bordeaux');
+-- => ORA-00001: violation de contrainte unique (INI3A03.PK_FOURNISSEUR)
+
+-- 2
+insert into FOURNISSEUR(nomfour, statut, ville) VALUES  ('VinBon' , 'SARL', 'Dijon');
+-- => insertion valide
+
+-- 3
+insert into FOURNISSEUR(nomfour, statut, ville) VALUES  ('VinBon' , 'SARL', 'Dijon');
+-- => ORA-00001: violation de contrainte unique (INI3A03.PK_FOURNISSEUR)
+
+-- 4
+insert into PRODUIT(appellation, nomfour, prix) values  ('Bordeaux' , 'Chapoutier', '20');
+-- ORA-00001: violation de contrainte unique (INI3A03.PK_PRODUIT)
