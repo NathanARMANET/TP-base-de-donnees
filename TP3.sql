512a71e39868e53f363173cd59e63216b3c91653
ARMANET Nathan <nathan.armanet@gmail.com>
2019-01-09 14:05:47 +0100

    requetes TP3

---
+++ TP3.sql
@@ -0,0 +1,10 @@
+-- TP3 : Transaction & PLSQL
+
+-- Partie 1 : Transactions
+
+-- 1. Atomicité d'une transaction courrantes
+
+-- Q.1
+
+
+-- 1. Plusieurs sessions sur un seul compte de BD et transactions coucurrentes
