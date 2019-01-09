2a9679a0ba3a810975a561bf22e687a4c3e239b6
ARMANET Nathan <nathan.armanet@gmail.com>
2019-01-09 14:23:06 +0100

    insertion modif et delete

--- TP3.sql
+++ TP3.sql
@@ -5,6 +5,21 @@
 -- 1. Atomicité d'une transaction courrantes
 
 -- Q.1
+create table test_TP3
+(
+	id int generated as identity
+		constraint test_TP3_pk
+			primary key,
+	valeur int not null
+);
 
+insert into test_TP3(valeur) values(2);
+insert into test_TP3(valeur) values(3);
+insert into test_TP3(valeur) values(6);
+insert into test_TP3(valeur) values(1);
+
+update TEST_TP3 set valeur = 7 where VALEUR = 3;
+
+delete from TEST_TP3 where ID = 12;         
 
 -- 1. Plusieurs sessions sur un seul compte de BD et transactions coucurrentes
