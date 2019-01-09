25f3c6ec2f998c751cafdca9fbf3b7ecbe542b2e
ARMANET Nathan <nathan.armanet@gmail.com>
2019-01-09 14:31:04 +0100

    question 2

--- TP3.sql
+++ TP3.sql
@@ -24,4 +24,20 @@ delete from TEST_TP3 where ID = 12;
 
 ROLLBACK --à enlever toutes les valeurs
 
+--Q.2
+insert into test_TP3(valeur) values(2);
+insert into test_TP3(valeur) values(3);
+insert into test_TP3(valeur) values(6);
+insert into test_TP3(valeur) values(1);
+
+delete from test_TP3 where valeur = 6;
+
+commit ;
+
+ROLLBACK; -- rien ne change seul la valeur 6 est supprimer, les autres restes
+
+delete from test_TP3 where id in (select id from test_TP3);
+
+commit;
+
 -- 1. Plusieurs sessions sur un seul compte de BD et transactions coucurrentes
